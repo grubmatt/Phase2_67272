939d0b1f217f778d92457b456ba06f41374cbf1b
Matt <mattgruber61@gmail.com>
2016-02-19 17:11:18 -0500

    Added additional scopes to assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,16 +8,23 @@ class Assignment < ActiveRecord::Base
   # -----------------------------
   # Gets current assignments
   scope :current, -> { where(active: true) }
+  # Gets past assignments
+  scope :past, -> { where(active: false) }
   # Returns all assignments for a given store
   scope :for_store, ->(store_id) { where("store_id = ?", store_id) }
   # Returns al assignments for a given employee
   scope :for_employee, ->(employee_id) { where("employee_id = ?", employee_id) }
-  # Return all asinments for a given pay level
+  # Return all assignments for a given pay level
   scope :for_pay_level, ->(pay_level) { where("pay_level = ?", pay_level) }
-  # Return all values ordered by pay level
-  scope :by_pay_level, -> { order('pay_level') }
+  # Return all assignments for a given role
+  scope :for_role, ->(role) { where("role = ?", role) }
   # Return all values ordered by store
   scope :by_store, -> { order('store_id') }
+  # Return all values ordered by employee name
+  scope :by_employee, -> { joins(:employee).order("employee.last_name, employee.first_name") }
+  # Return assignments in chronological order
+  scope :chronological, -> { order("start_date DSC") }
+ 
 
   # Validations
   # -----------------------------
