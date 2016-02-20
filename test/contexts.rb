2b61419142eb6aa2f822f0598435c2e161ba8d51
Matt <mattgruber61@gmail.com>
2016-02-19 22:18:17 -0500

    Created contexts

---
+++ test/contexts.rb
@@ -0,0 +1,43 @@
+module Contexts
+
+  def create_employees
+  	@ty = FactoryGirl.create(:employee)
+  	@terry = FactoryGirl.create(:employee, first_name: "Terry", date_of_birth: 1990-2-1, role:"manager")
+  	@james = FactoryGirl.create(:employee, first_name: "James", active: false)
+  	@alex = FactoryGirl.create(:employee, first_name: "Alex", last_name: "Heimann", role:"admin")
+  end
+
+  def create_stores
+  	@cmu = FactoryGirl.create(:store)
+  	@pitt = FactoryGirl.create(:store, name: "PITT")
+  	@mars = FactoryGirl.create(:store, name: "MARS TWP", active: false)
+  end
+
+  def create_assignments
+  	@cmu_ty = FactoryGirl.create(:assignment, start_date: 2016-1-5, pay_level: 2)
+  	@cmu_ty2 = FactoryGirl.create(:assignment, end_date: 2016-1-5)
+  	@mars_alex = FactoryGirl.create(:assignment, start_date: 2014-1-5, end_date: 2015-3-1, store_id: 3)
+ 	@pitt_terry = FactoryGirl.create(:assignment, start_date: 2015-10-9)
+  end
+
+  def destroy_employees
+  	@ty.destroy
+  	@terry.destroy
+  	@james.destroy
+  	@alex.destroy
+  end
+
+  def destroy_stores
+  	@cmu.destroy
+  	@pitt.destroy
+  	@mars.destroy
+  end
+
+  def destroy_assignments
+  	@cmu_ty.destroy
+  	@cmu_ty2.destroy
+  	@mars_alex.destroy
+  	@pitt_terry.destroy
+  end
+
+end
--- test/factories.rb
+++ test/factories.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :employee do
     first_name "Tyler"
     last_name "Toboggan"
-    date_of_birth 1994-01-23
+    date_of_birth 2000-01-23
     role "employee"
     ssn "023649320"
   end
@@ -12,11 +12,11 @@ FactoryGirl.define do
     store_id 1
     employee_id 1
     start_date 2015-07-15
-    pay_level 2
+    pay_level 1
   end
 
   factory :store do
-  	name "UC CMU"
+  	name "CMU"
 	street "340 Forbes Avenue"
 	zip 15217 
   end
